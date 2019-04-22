618b27042d84a2c2bfc45d7c4290d654de0ad63c
Alex <alexiv1102@gmail.com>
2019-04-22 14:48:00 +0300

    sort by id

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,68 @@
 import React, {Component} from "react";
 import "./style.scss";
-import {Input} from "../Input"
 import {List} from "../List"
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {items: [], value: ""};
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.sortById = this.sortById.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value});
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        if (!this.state.value.length) {
+            return;
+        }
+
+        const newItem = {
+            value: this.state.value,
+            id: this.state.items.length + 1
+        };
+
+
+        this.setState(state => ({
+            items: state.items.concat(newItem),
+            value: ""
+        }));
+        // console.log(this.state)
+        console.log(this.state.items.length)
+
+    }
+
+    sortById() {
+        console.log(this.state.items);
+        this.setState(this.state.items.reverse());
+        console.log(this.state.items);
+        this.render();
+    }
+
     render() {
         return (
             <div className="container">
                 <header className="header">TODO List</header>
-                <Input/>
-                <List/>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input className="input" type="text" value={this.state.value} onChange={this.handleChange}/>
+                        <button className="btn btn-add">&#10010;</button>
+                    </label>
+                </form>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th onClick={this.sortById}>#</th>
+                        <th>Name</th>
+                    </tr>
+                    </thead>
+                    <List items={this.state.items}/>
+                </table>
             </div>
         );
     }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,29 +2,26 @@ import React, {Component} from "react";
 import "./style.scss";
 
 class List extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+        // console.log(this.props.items);
         return (
-            <table className="table">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <th>1</th>
-                    <td>First todoffffffffffff ffffffffff ffffffff fffffff</td>
+            <tbody>
+            {this.props.items.map(item =>
+                <tr key={item.id}>
+                    <th>{item.id}</th>
+                    <td>{item.value}</td>
                     <td>
                         <button className="btn btn-del">&#10006;</button>
                     </td>
                 </tr>
-                </tbody>
-
-            </table>
-        );
+            )}
+            </tbody>
+        )
     }
-
 }
 
 export default List;
