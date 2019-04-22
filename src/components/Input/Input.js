d87fafd8947588561c22c4c280da26fb51969b3f
Alex <alexiv1102@gmail.com>
2019-04-22 10:16:59 +0300

    value from form to Input state

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,13 +1,43 @@
 import React, {Component} from "react";
-import './style.scss'
+import "./style.scss"
 
 class Input extends Component {
+    constructor() {
+        super();
+        this.state = {items: [], value: ""};
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value});
+    }
+
+    handleSubmit(e) {
+        console.log(this.state.value);
+        e.preventDefault();
+
+        if (!this.state.value.length) {
+            return;
+        }
+
+        const newItem = {value: this.state.value};
+
+        this.setState(state => ({
+            items: state.items.concat(newItem),
+            value: ""
+        }));
+
+    }
+
     render() {
         return (
-            <label>
-                <input className="input" type="text"/>
-                <button className="btn btn-add">&#10010;</button>
-            </label>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    <input className="input" type="text" value={this.state.value} onChange={this.handleChange}/>
+                    <button className="btn btn-add">&#10010;</button>
+                </label>
+            </form>
         );
     }
 }
