b5299ebfbecaa909e608d491e7b80cd0256d4eec
Alex <alexiv1102@gmail.com>
2019-04-22 17:51:51 +0300

    Updates

---
+++ precache-manifest.573cfcd634265ab5b1935fc86fc7bbae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e03235dbed8696c067c",
+    "url": "/todo-list-react/static/js/runtime~main.f99d2fcc.js"
+  },
+  {
+    "revision": "5f63950840d4892ce5b2",
+    "url": "/todo-list-react/static/js/main.0b3fb16b.chunk.js"
+  },
+  {
+    "revision": "9697d3492f0986005fda",
+    "url": "/todo-list-react/static/js/2.810db632.chunk.js"
+  },
+  {
+    "revision": "5f63950840d4892ce5b2",
+    "url": "/todo-list-react/static/css/main.c200912a.chunk.css"
+  },
+  {
+    "revision": "a0aa178b137798afab17e6810627d269",
+    "url": "/todo-list-react/index.html"
+  }
+];
